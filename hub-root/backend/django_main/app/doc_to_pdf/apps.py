100dc3549eda63744f26e47869cf1ac1001db92a
rhsajib <sajib.mmebuet@gmail.com>
2023-12-12 20:47:21 +0600

    deleted doc_to_pdf and modified requirements.txt

--- hub-root/backend/django_main/app/doc_to_pdf/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocToPdfConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'doc_to_pdf'
--- hub-root/backend/django_main/app/doc_to_pdf/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import doc_to_pdf_view
-
-app_name = 'doc_to_pdf'
-
-urlpatterns = [
-    path('', doc_to_pdf_view, name='doc_to_pdf_view'),
-]
